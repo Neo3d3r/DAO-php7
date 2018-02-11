097e9f2db45b41f8532f75a55523e13eab7b8a6a
unknown <neno.smg@hotmail.com>
2018-02-11 14:50:46 -0300

    Metodos de listar

--- index.php
+++ index.php
@@ -1,10 +1,28 @@
 <?php
 
 require_once ("config.php");
+//Carrega somente 1 usuario
+//$root = new Usuario();
 
-$root = new Usuario();
+//$root->loadById(2);
 
-$root->loadById(2);
+//echo $root;
 
-echo $root;
+//Carrega uma lista de usuários
 
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+//Carrega uma lista buscando pelo login
+
+//$search = Usuario::search("us");
+
+//echo json_encode($search);
+
+//Carrega um usuário pelo Login e Senha - Autenticação
+
+$usuario = new Usuario();
+$usuario->login("root", "12345");
+
+echo $usuario;
