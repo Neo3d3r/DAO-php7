72a43991481effdebf2414f8c8575bfb18a848bd
unknown <neno.smg@hotmail.com>
2018-02-11 14:21:46 -0300

    Criamos a classe usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,16 +1,76 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Usuario
  *
  * @author Eder
  */
 class Usuario {
-    //put your code here
+
+    private $idusuario;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
+
+    function getIdusuario() {
+        return $this->idusuario;
+    }
+
+    function getDeslogin() {
+        return $this->deslogin;
+    }
+
+    function getDessenha() {
+        return $this->dessenha;
+    }
+
+    function getDtcadastro() {
+        return $this->dtcadastro;
+    }
+
+    function setIdusuario($idusuario) {
+        $this->idusuario = $idusuario;
+    }
+
+    function setDeslogin($deslogin) {
+        $this->deslogin = $deslogin;
+    }
+
+    function setDessenha($dessenha) {
+        $this->dessenha = $dessenha;
+    }
+
+    function setDtcadastro($dtcadastro) {
+        $this->dtcadastro = $dtcadastro;
+    }
+
+    public function loadById($id) {
+
+        $sql = new Sql();
+
+        $result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID" => $id
+        ));
+
+        if (count($result) > 0) {
+
+            $row = $result[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }
+    }
+
+    public function __toString() {
+
+        return json_encode(array(
+            "idusuario" => $this->getIdusuario(),
+            "deslogin" => $this->getDeslogin(),
+            "dessenha" => $this->getDessenha(),
+            "dtcadastro" => $this->getDtcadastro()->format("d/m/Y H:i:s")
+        ));
+    }
+
 }
