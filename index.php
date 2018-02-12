518d2f1a42beb7df26b4bba5ac3e10e5225f0d93
unknown <neno.smg@hotmail.com>
2018-02-12 13:34:48 -0300

    atualização no usuario

--- index.php
+++ index.php
@@ -3,26 +3,40 @@
 require_once ("config.php");
 //Carrega somente 1 usuario
 //$root = new Usuario();
-
 //$root->loadById(2);
-
 //echo $root;
 
 //Carrega uma lista de usuários
-
 //$lista = Usuario::getList();
-
 //echo json_encode($lista);
-
 //Carrega uma lista buscando pelo login
-
 //$search = Usuario::search("us");
-
 //echo json_encode($search);
 
 //Carrega um usuário pelo Login e Senha - Autenticação
+//$usuario = new Usuario();
+//$usuario->login("root", "12345");
+//echo $usuario;
 
+/*
+ * /Insere um usuario no Bando de Dados
+
+$aluno = new Usuario("Aluno", "654321");
+$aluno->insert();
+echo $aluno;
+ * 
+ */
+
+/* Atualiza um usuario no banco UPDATE
 $usuario = new Usuario();
-$usuario->login("root", "12345");
+$usuario->loadById(7);
+$usuario->update("Professor", "568974");
+echo $usuario;
+ * 
+ */
 
+//Deleta um usuario do banco de dados DELETE
+$usuario = new Usuario();
+$usuario->loadById(6);
+$usuario->delete();
 echo $usuario;
